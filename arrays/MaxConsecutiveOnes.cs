44cefcc9d535e82a88aad63e394af7a9ee8cd5c2
GilmoS <gil.moshe23@gmail.com>
2026-01-13 11:52:27 +0200

    Given a binary array nums, return the maximum number of consecutive 1's in the array.

---
+++ arrays/MaxConsecutiveOnes.cs
@@ -0,0 +1,26 @@
+public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int max = 0;
+        int curr = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                curr++;
+            }
+            else
+            {
+                curr = 0;
+            }
+            if (curr > max)
+            {
+                max = curr;
+            }
+
+        }
+        return max;
+
+    }
+}
